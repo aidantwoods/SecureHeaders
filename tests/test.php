bc08dc2477ef5d9b5940be535fc94d1feb0150d3
Aidan Woods <aidantwoods@gmail.com>
2016-11-25 23:34:17 +0000

    elementary testing

--- tests/test.php
+++ tests/test.php
@@ -1,8 +1,73 @@
 <?php
+
+include('SecureHeaders.php');
+
 class Test extends PHPUnit_Framework_TestCase
 {
-	public function testOnePlusOne() {
-		$this->assertEquals(1,1);
+	private $assertions = array(
+		'Contains',
+		'NotContains',
+		'Equals',
+		'Regexp'
+	);
+
+	function data_safe_mode()
+	{
+		return array(
+			[
+				'test' => 
+					function(&$headers){
+						$headers->header('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
+					},
+				'assertions' => [
+					'Contains' => 'Strict-Transport-Security: max-age=31536000; includeSubDomains; preload'
+				]
+			],
+			[
+				'test' => 
+					function(&$headers){
+						$headers->safe_mode();
+						$headers->header('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
+					},
+				'assertions' => [
+					'NotContains' => 'Strict-Transport-Security: max-age=31536000; includeSubDomains; preload',
+					'Contains' => 'Strict-Transport-Security: max-age=86400'
+				]
+			],
+			[
+				'test' => 
+					function(&$headers){
+						$headers->safe_mode();
+						$headers->header('Public-Key-Pins', 'max-age=31536000; pin-sha256="abcd"; includeSubDomains');
+					},
+				'assertions' => [
+					'NotContains' => 'max-age=31536000; pin-sha256="abcd"; includeSubDomains',
+					'Contains' => 'Public-Key-Pins: max-age=10; pin-sha256="abcd"'
+				]
+			]
+		);
+	}
+
+	/**
+     * @dataProvider data_safe_mode
+	 * @param $test
+	 * @param $assertions
+     */
+	public function test_safe_mode($test, $assertions)
+	{
+		$headers = new SecureHeaders;
+		$headers->headers_as_string(true);
+		$test($headers);
+
+		$headers_string = $headers->get_headers_as_string();
+
+		foreach ($this->assertions as $assertion)
+		{
+			if (isset($assertions[$assertion]))
+			{
+				$this->{'assert'.$assertion}($assertions[$assertion], $headers_string);
+			}
+		}
   	}
-}
+}   
 ?>
