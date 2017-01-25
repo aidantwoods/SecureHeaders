03d20bc756599996b9752e25c86a138fed165bda
Franz Liedke <franz@develophp.org>
2017-01-26 00:38:39 +0100

    Add some more tests for default behavior

--- tests/SecureHeadersTest.php
+++ tests/SecureHeadersTest.php
@@ -31,6 +31,55 @@ class SecureHeadersTest extends PHPUnit_Framework_TestCase
         $this->assertContains('X-Foo: Bar', $headersString);
     }
 
+    public function testThreeDefaultHeadersAreAdded()
+    {
+        $headerStrings = new StringHttpAdapter;
+
+        $headers = new SecureHeaders($headerStrings);
+        $headers->errorReporting(false);
+        $headers->done();
+
+        $headersString = $headerStrings->getSentHeaders();
+
+        $this->assertContains('X-XSS-Protection: 1; mode=block', $headersString);
+        $this->assertContains('X-Content-Type-Options: nosniff', $headersString);
+        $this->assertContains('X-Frame-Options: Deny', $headersString);
+    }
+
+    public function testDefaultHeadersDoNotReplaceExistingHeaders()
+    {
+        $headerStrings = new StringHttpAdapter(array(
+            'X-Frame-Options: sameorigin',
+        ));
+
+        $headers = new SecureHeaders($headerStrings);
+        $headers->errorReporting(false);
+        $headers->done();
+
+        $headersString = $headerStrings->getSentHeaders();
+
+        $this->assertContains('X-XSS-Protection: 1; mode=block', $headersString);
+        $this->assertContains('X-Content-Type-Options: nosniff', $headersString);
+        $this->assertContains('X-Frame-Options: sameorigin', $headersString);
+        $this->assertNotContains('X-Frame-Options: Deny', $headersString);
+    }
+
+    public function testDefaultHeadersCanBeExplicitlyRemoved()
+    {
+        $headerStrings = new StringHttpAdapter;
+
+        $headers = new SecureHeaders($headerStrings);
+        $headers->errorReporting(false);
+        $headers->removeHeader('X-XSS-Protection');
+        $headers->done();
+
+        $headersString = $headerStrings->getSentHeaders();
+
+        $this->assertContains('X-Content-Type-Options: nosniff', $headersString);
+        $this->assertContains('X-Frame-Options: Deny', $headersString);
+        $this->assertNotContains('X-XSS-Protection', $headersString);
+    }
+
     function dataSafeMode()
     {
         return array(
