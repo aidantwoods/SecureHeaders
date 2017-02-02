23dda850546201d6810ca38fbb0f577c9dacca1d
Franz Liedke <franz@develophp.org>
2017-02-02 19:09:23 +0100

    Implement and test PSR-7 adapter

---
+++ tests/Http/Psr7AdapterTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tests\Http;
+
+use Aidantwoods\SecureHeaders\HeaderBag;
+use Aidantwoods\SecureHeaders\Http\Psr7Adapter;
+use PHPUnit_Framework_TestCase;
+use Zend\Diactoros\Response;
+
+class Psr7AdapterTest extends PHPUnit_Framework_TestCase
+{
+    public function testProperlyFillsHeaderBag()
+    {
+        $response = (new Response())
+            ->withAddedHeader('Content-Type', 'text/html')
+            ->withAddedHeader('X-Foo-Bar', 'val1')
+            ->withAddedHeader('X-Foo-Bar', 'val2');
+
+        $adapter = new Psr7Adapter($response);
+        $headers = $adapter->getHeaders();
+
+        $this->assertTrue($headers->has('content-type'));
+        $this->assertTrue($headers->has('x-foo-bar'));
+
+        $this->assertCount(3, $headers->get());
+    }
+
+    public function testRemovesAllPreviousHeadersFromResponse()
+    {
+        $response = (new Response())
+            ->withAddedHeader('Content-Type', 'text/html')
+            ->withAddedHeader('X-Foo-Bar', 'val1')
+            ->withAddedHeader('X-Foo-Bar', 'val2');
+
+        $adapter = new Psr7Adapter($response);
+        $adapter->sendHeaders(new HeaderBag());
+
+        $finalResponse = $adapter->getFinalResponse();
+
+        $this->assertEquals(array(), $finalResponse->getHeaders());
+    }
+
+    public function testSendsAllHeadersFromHeaderBag()
+    {
+        $response = (new Response())
+            ->withAddedHeader('Content-Type', 'text/html')
+            ->withAddedHeader('X-Foo-Bar', 'val1')
+            ->withAddedHeader('X-Foo-Bar', 'val2');
+
+        $adapter = new Psr7Adapter($response);
+        $adapter->sendHeaders(HeaderBag::fromHeaderLines(array(
+            'Content-Type: text/xml',
+            'Content-Length: 123',
+            'Cache-Control: no-worries :)'
+        )));
+
+        $finalResponse = $adapter->getFinalResponse();
+
+        $this->assertEquals(array(
+            'content-type' => array('text/xml'),
+            'content-length' => array('123'),
+            'cache-control' => array('no-worries :)')
+        ), $finalResponse->getHeaders());
+    }
+}   
