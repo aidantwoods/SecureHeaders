e5f5adfa820e5e05276f96378272efb7eabfa775
Aidan Woods <aidantwoods@gmail.com>
2017-02-08 17:29:55 +0000

    move description of function out of param

--- src/SecureHeaders.php
+++ src/SecureHeaders.php
@@ -1319,40 +1319,38 @@ class SecureHeaders{
     # public functions: general
 
     /**
-     * Description
+     * Calling this function will initiate the following
      *
-     * @param HttpAdapter $http = new GlobalHttpAdapter
-     *  An implementation of the {@see HttpAdapter} interface, to which
-     *  settings configured via SecureHeaders will be applied.
-     *
-     *  Calling this function will initiate the following
-     *
-     *  1. Existing headers from the HttpAdapter's source will be imported into
-     *     SecureHeaders' internal list, parsed
-     *  2. [Automatic header functions](auto) will be applied
-     *  3. [CSP](csp), [HSTS](hsts), and [HPKP](hpkp) policies will be compiled
-     *     and added to SecureHeaders' internal header list
-     *  4. Headers queued for [removal](removeHeader) will be deleted from
-     *     SecureHeaders' internal header list
-     *  5. [Safe Mode](safeMode) will examine the list of headers, and make any
-     *     required changes according to its settings
-     *  6. The HttpAdapter will be instructed to remove all headers from its
-     *     header source, Headers will then be copied from SecureHeaders'
-     *     internal header list, into the HttpAdapter's (now empty) list of
-     *     headers
-     *  7. If [error reporting](errorReporting) is enabled (both within
-     *     SecureHeaders and according to the PHP configuration values for
-     *     error reporting, and whether to display errors)
-     *     * Missing security headers will be reported as `E_USER_WARNING`
-     *     * Misconfigured headers will be reported as `E_USER_WARNING` or
-     *       `E_USER_NOTICE` depending on severity, the former being most
-     *       severe an issue.
+     * 1. Existing headers from the HttpAdapter's source will be imported into
+     *    SecureHeaders' internal list, parsed
+     * 2. [Automatic header functions](auto) will be applied
+     * 3. [CSP](csp), [HSTS](hsts), and [HPKP](hpkp) policies will be compiled
+     *    and added to SecureHeaders' internal header list
+     * 4. Headers queued for [removal](removeHeader) will be deleted from
+     *    SecureHeaders' internal header list
+     * 5. [Safe Mode](safeMode) will examine the list of headers, and make any
+     *    required changes according to its settings
+     * 6. The HttpAdapter will be instructed to remove all headers from its
+     *    header source, Headers will then be copied from SecureHeaders'
+     *    internal header list, into the HttpAdapter's (now empty) list of
+     *    headers
+     * 7. If [error reporting](errorReporting) is enabled (both within
+     *    SecureHeaders and according to the PHP configuration values for
+     *    error reporting, and whether to display errors)
+     *    * Missing security headers will be reported as `E_USER_WARNING`
+     *    * Misconfigured headers will be reported as `E_USER_WARNING` or
+     *      `E_USER_NOTICE` depending on severity, the former being most
+     *      severe an issue.
      *
      *  **Note:** Calling this function is **required** before the first byte
      *  of output in order for SecureHeaders to (be able to) do anything. If
      *  you're not sure when the first byte of output might occur, or simply
      *  don't want to have to call this every time – take a look at
      *  {@see applyOnOutput} to have SecureHeaders take care of this for you.
+     *
+     * @param HttpAdapter $http = new GlobalHttpAdapter
+     *  An implementation of the {@see HttpAdapter} interface, to which
+     *  settings configured via SecureHeaders will be applied.
      *
      * @return HeaderBag
      *  Returns the headers
