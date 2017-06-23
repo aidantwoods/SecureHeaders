0a290d54e925582216b71bd6efdf94aaa6e91566
Aidan Woods <aidantwoods@gmail.com>
2017-06-23 19:07:50 +0100

    add expect-ct tests

--- tests/SecureHeadersTest.php
+++ tests/SecureHeadersTest.php
@@ -31,7 +31,7 @@ class SecureHeadersTest extends PHPUnit_Framework_TestCase
         $this->assertContains('X-Foo: Bar', $headersString);
     }
 
-    public function testFourDefaultHeadersAreAdded()
+    public function testSevenDefaultHeadersAreAdded()
     {
         $headerStrings = new StringHttpAdapter;
 
@@ -41,6 +41,9 @@ class SecureHeadersTest extends PHPUnit_Framework_TestCase
 
         $headersString = $headerStrings->getSentHeaders();
 
+        $this->assertContains('Expect-CT: max-age=0', $headersString);
+        # we want to ensure ordering here, hence two header test
+        $this->assertContains("Referrer-Policy: no-referrer\nReferrer-Policy: strict-origin-when-cross-origin", $headersString);
         $this->assertContains('X-Permitted-Cross-Domain-Policies: none', $headersString);
         $this->assertContains('X-XSS-Protection: 1; mode=block', $headersString);
         $this->assertContains('X-Content-Type-Options: nosniff', $headersString);
@@ -59,6 +62,9 @@ class SecureHeadersTest extends PHPUnit_Framework_TestCase
 
         $headersString = $headerStrings->getSentHeaders();
 
+        $this->assertContains('Expect-CT: max-age=0', $headersString);
+        # we want to ensure ordering here, hence two header test
+        $this->assertContains("Referrer-Policy: no-referrer\nReferrer-Policy: strict-origin-when-cross-origin", $headersString);
         $this->assertContains('X-Permitted-Cross-Domain-Policies: none', $headersString);
         $this->assertContains('X-XSS-Protection: 1; mode=block', $headersString);
         $this->assertContains('X-Content-Type-Options: nosniff', $headersString);
@@ -77,6 +83,9 @@ class SecureHeadersTest extends PHPUnit_Framework_TestCase
 
         $headersString = $headerStrings->getSentHeaders();
 
+        $this->assertContains('Expect-CT: max-age=0', $headersString);
+        # we want to ensure ordering here, hence two header test
+        $this->assertContains("Referrer-Policy: no-referrer\nReferrer-Policy: strict-origin-when-cross-origin", $headersString);
         $this->assertContains('X-Permitted-Cross-Domain-Policies: none', $headersString);
         $this->assertContains('X-Content-Type-Options: nosniff', $headersString);
         $this->assertContains('X-Frame-Options: Deny', $headersString);
