b6cc1302ca055c64b4cd04a1b2d98b9503d7bb5b
Aidan Woods <aidantwoods@gmail.com>
2017-08-24 00:00:56 +0100

    Test new config

--- tests/Operations/InjectStrictDynamicTest.php
+++ tests/Operations/InjectStrictDynamicTest.php
@@ -8,13 +8,15 @@ use PHPUnit_Framework_TestCase;
 
 class InjectStrictDynamicTest extends PHPUnit_Framework_TestCase
 {
+    private $algs = ['sha256', 'sha384', 'sha512'];
+
     public function testSDInjectedInDefaultSrc()
     {
         $HeaderBag = HeaderBag::fromHeaderLines([
             "Content-Security-Policy: default-src 'nonce-1234'",
         ]);
 
-        $InjSD = new InjectStrictDynamic(['sha256', 'sha384', 'sha512']);
+        $InjSD = new InjectStrictDynamic($this->algs, InjectStrictDynamic::ENFORCE | InjectStrictDynamic::REPORT);
         $InjSD->modify($HeaderBag);
 
         $this->assertCount(1, $HeaderBag->get());
@@ -30,7 +32,7 @@ class InjectStrictDynamicTest extends PHPUnit_Framework_TestCase
             "Content-Security-Policy: default-src 'sha256-1234'",
         ]);
 
-        $InjSD = new InjectStrictDynamic(['sha256', 'sha384', 'sha512']);
+        $InjSD = new InjectStrictDynamic($this->algs, InjectStrictDynamic::ENFORCE | InjectStrictDynamic::REPORT);
         $InjSD->modify($HeaderBag);
 
         $this->assertCount(1, $HeaderBag->get());
@@ -46,7 +48,7 @@ class InjectStrictDynamicTest extends PHPUnit_Framework_TestCase
             "Content-Security-Policy: script-src 'nonce-1234'",
         ]);
 
-        $InjSD = new InjectStrictDynamic(['sha256', 'sha384', 'sha512']);
+        $InjSD = new InjectStrictDynamic($this->algs, InjectStrictDynamic::ENFORCE | InjectStrictDynamic::REPORT);
         $InjSD->modify($HeaderBag);
 
         $this->assertCount(1, $HeaderBag->get());
@@ -62,7 +64,7 @@ class InjectStrictDynamicTest extends PHPUnit_Framework_TestCase
             "Content-Security-Policy: script-src 'sha256-1234'",
         ]);
 
-        $InjSD = new InjectStrictDynamic(['sha256', 'sha384', 'sha512']);
+        $InjSD = new InjectStrictDynamic($this->algs, InjectStrictDynamic::ENFORCE | InjectStrictDynamic::REPORT);
         $InjSD->modify($HeaderBag);
 
         $this->assertCount(1, $HeaderBag->get());
@@ -78,7 +80,7 @@ class InjectStrictDynamicTest extends PHPUnit_Framework_TestCase
             "Content-Security-Policy: default-src 'nonce-1234'; script-src 'nonce-abcd'",
         ]);
 
-        $InjSD = new InjectStrictDynamic(['sha256', 'sha384', 'sha512']);
+        $InjSD = new InjectStrictDynamic($this->algs, InjectStrictDynamic::ENFORCE | InjectStrictDynamic::REPORT);
         $InjSD->modify($HeaderBag);
 
         $this->assertCount(1, $HeaderBag->get());
@@ -94,7 +96,7 @@ class InjectStrictDynamicTest extends PHPUnit_Framework_TestCase
             "Content-Security-Policy: default-src 'nonce-1234'; script-src http://example.com",
         ]);
 
-        $InjSD = new InjectStrictDynamic(['sha256', 'sha384', 'sha512']);
+        $InjSD = new InjectStrictDynamic($this->algs, InjectStrictDynamic::ENFORCE | InjectStrictDynamic::REPORT);
         $InjSD->modify($HeaderBag);
 
         $this->assertCount(1, $HeaderBag->get());
@@ -102,5 +104,59 @@ class InjectStrictDynamicTest extends PHPUnit_Framework_TestCase
         $headerString = (string) $HeaderBag->get()[0];
 
         $this->assertSame("Content-Security-Policy: default-src 'nonce-1234'; script-src http://example.com", $headerString);
+    }
+
+    public function testSDInjectedInCSPRO()
+    {
+        $HeaderBag = HeaderBag::fromHeaderLines([
+            "Content-Security-Policy-Report-Only: default-src 'nonce-1234'",
+        ]);
+
+        $InjSD = new InjectStrictDynamic($this->algs, InjectStrictDynamic::ENFORCE | InjectStrictDynamic::REPORT);
+        $InjSD->modify($HeaderBag);
+
+        $this->assertCount(1, $HeaderBag->get());
+
+        $headerString = (string) $HeaderBag->get()[0];
+
+        $this->assertSame("Content-Security-Policy-Report-Only: default-src 'nonce-1234' 'strict-dynamic'", $headerString);
+    }
+
+    public function testSDInjectedInCSPROExclusive()
+    {
+        $HeaderBag = HeaderBag::fromHeaderLines([
+            "Content-Security-Policy: default-src 'nonce-1234'",
+            "Content-Security-Policy-Report-Only: default-src 'nonce-1234'",
+        ]);
+
+        $InjSD = new InjectStrictDynamic($this->algs, InjectStrictDynamic::REPORT);
+        $InjSD->modify($HeaderBag);
+
+        $this->assertCount(2, $HeaderBag->get());
+
+        $this->assertNotContains('strict-dynamic', (string) $HeaderBag->get()[0]);
+
+        $headerString = (string) $HeaderBag->get()[1];
+
+        $this->assertSame("Content-Security-Policy-Report-Only: default-src 'nonce-1234' 'strict-dynamic'", $headerString);
+    }
+
+    public function testSDInjectedInCSPExclusive()
+    {
+        $HeaderBag = HeaderBag::fromHeaderLines([
+            "Content-Security-Policy: default-src 'nonce-1234'",
+            "Content-Security-Policy-Report-Only: default-src 'nonce-1234'",
+        ]);
+
+        $InjSD = new InjectStrictDynamic($this->algs, InjectStrictDynamic::ENFORCE);
+        $InjSD->modify($HeaderBag);
+
+        $this->assertCount(2, $HeaderBag->get());
+
+        $this->assertNotContains('strict-dynamic', (string) $HeaderBag->get()[1]);
+
+        $headerString = (string) $HeaderBag->get()[0];
+
+        $this->assertSame("Content-Security-Policy: default-src 'nonce-1234' 'strict-dynamic'", $headerString);
     }
 }
